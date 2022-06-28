348daef703daf34d1545ed74ffad8a41af299f5c
Mohamed Abdel-Samie <esmaelm885@gmail.com>
2022-06-29 00:52:13 +0200

    sh

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,13 +1,12 @@
 cd ./www
-echo 'Start deploying'
+echo '======== Start deploying ======='
 echo $EB_APP 
 echo $AWS_DEFAULT_REGION
 eb init $EB_APP --region $AWS_DEFAULT_REGION --platform node.js
 eb use $EB_ENV
-echo $EB_ENV
 echo 'set evn variables'
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION URL=$URL JWT_SECRET=$JWT_SECRET
-echo 'Arch app'
-zip -r Archive.zip .
+echo '+++++++ EB List +++++++++'
+eb list
 echo 'deploy'
 eb deploy
